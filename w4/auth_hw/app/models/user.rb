4f49614d2771e2acaaec990df167b690e8702953
Alex Doundakov <adoundakov@gmail.com>
2016-10-04 21:40:52 -0700

    Finish auth exercises for rails

--- w4/auth_hw/app/models/user.rb
+++ w4/auth_hw/app/models/user.rb
@@ -9,29 +9,36 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
+require 'bcrypt'
 
 class User < ApplicationRecord
-  before_validation :ensure_session_token
   validates :username, presence: true, uniqueness: true
-  validates :password_digest, presence: true, length: { 6, allow_nil: true }
+  validates :password_digest, presence: { message: "Password can't be blank "}
+  validates :password, presence: true, length: { minimum: 6, allow_nil: true }
+  validates :session_token, presence: true, uniqueness: true
+  before_validation :ensure_session_token
 
+  attr_reader :password
   def self.find_by_credentials(username, password)
-
+    u =  User.find_by(username: username)
+    return u if u && BCrypt::Password.new(u.password_digest).is_password?(password)
   end
 
   def self.generate_session_token
-
+    SecureRandom::urlsafe_base64
   end
 
   def reset_session_token!
-
+    self.session_token = User.generate_session_token
+    self.save!
   end
 
   def ensure_session_token
-
+    self.session_token ||= User.generate_session_token
   end
 
-  def password=
-
+  def password=(pass)
+    @password = pass
+    self.password_digest = BCrypt::Password.create(@password)
   end
 end
