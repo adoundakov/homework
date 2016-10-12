8f22ce221726928bfd27f5a703744d27f66c166a
Alex Doundakov <adoundakov@gmail.com>
2016-10-11 17:00:49 -0700

    finish ar lite

--- w3/ar_lite/lib/03_associatable.rb
+++ w3/ar_lite/lib/03_associatable.rb
@@ -1,16 +1,12 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
+require 'byebug'
 
-# Phase IIIa
 class AssocOptions
-  attr_accessor(
-    :foreign_key,
-    :class_name,
-    :primary_key
-  )
+  attr_accessor :foreign_key, :class_name, :primary_key
 
   def model_class
-    class_name.constantize
+    @class_name.constantize
   end
 
   def table_name
@@ -20,67 +16,64 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    if options[:primary_key]
-      @primary_key = options[:primary_key]
-    else
-      @primary_key = :id
-    end
+    defaults = {
+      :foreign_key => "#{name}_id".to_sym,
+      :class_name => name.to_s.camelcase,
+      :primary_key => :id
+    }
 
-    if options[:foreign_key]
-      @foreign_key = options[:foreign_key]
-    else
-      @foreign_key = "#{name}_id".to_sym
-    end
-
-    if options[:class_name]
-      @class_name = options[:class_name]
-    else
-      @class_name = "#{name}".camelcase
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
     end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    if options[:primary_key]
-      @primary_key = options[:primary_key]
-    else
-      @primary_key = :id
-    end
+    defaults = {
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym,
+      :class_name => name.to_s.singularize.camelcase,
+      :primary_key => :id
+    }
 
-    if options[:foreign_key]
-      @foreign_key = options[:foreign_key]
-    else
-      @foreign_key = "#{self_class_name.downcase}_id".to_sym
-    end
-
-    if options[:class_name]
-      @class_name = options[:class_name]
-    else
-      @class_name = "#{name}".singularize.camelcase
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
     end
   end
 end
 
 module Associatable
-  # Phase IIIb
   def belongs_to(name, options = {})
-    options = BelongsToOptions.new(name, options)
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+
     define_method(name) do
-      f_val = send(options.foreign_key)
-      result = DBConnection.execute(<<-SQL, f_val)
-      SELECT * FROM #{self.class.table_name} WHERE #{options.primary_key} = ?
-      SQL
-      result.first
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.foreign_key)
+      options
+        .model_class
+        .where(options.primary_key => key_val)
+        .first
     end
   end
 
   def has_many(name, options = {})
-    # ...
+    self.assoc_options[name] =
+      HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.primary_key)
+      options
+        .model_class
+        .where(options.foreign_key => key_val)
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
