253b2e88aebda64c85cb442b435353c1eb9791b3
Alex Doundakov <adoundakov@gmail.com>
2016-10-25 08:04:11 -0700

    finish middlewares homework

--- w7/day2/solution/frontend/store/store.js
+++ w7/day2/solution/frontend/store/store.js
@@ -1,20 +1,30 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
-
 
 const preloadedState = {
   notes: [],
   tracks: {},
   isRecording:false,
   isPlaying:false
-}
+};
 
 
 const configureStore = (state = preloadedState) => (
   createStore(
     rootReducer,
-    state
+    state,
+    applyMiddleware(logger)
   )
 );
+
+function logger({getState}) {
+  return next => action => {
+    console.log(getState());
+    console.log('dispatching', action);
+    let result = next(action);
+    console.log('after dispatch', getState());
+    return result;
+  };
+}
 
 export default configureStore;
--- w7/day2/solution/frontend/synthesizer.jsx
+++ w7/day2/solution/frontend/synthesizer.jsx
@@ -8,6 +8,26 @@ import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
+  // const newStore = applyMiddlewares(store, addLoggingToDispatch);
   const rootEl = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, rootEl);
 });
+
+
+// not going to be used once we implement redux's middlewares function
+const addLoggingToDispatch = store => next => action => {
+  let dispatch = store.dispatch;
+  console.log(store.getState());
+  console.log(action);
+  let result = dispatch(action);
+  console.log(store.getState());
+  return result;
+};
+
+const applyMiddlewares = (store, ...middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach((middleware) => {
+    dispatch = middleware(store)(dispatch);
+  });
+  return Object.assign({}, store, {dispatch});
+};
